7ef49422a529d44d7a3cbf7e501f076a575e8706
Jahid Uddin <jahidudd65@gmail.com>
2026-02-14 19:01:54 +0100

    [init]: implement dark mode

---
+++ src/main/java/de/ju/jict/App.java
@@ -0,0 +1,61 @@
+package de.ju.jict;
+
+import imgui.app.Application;
+import imgui.app.Configuration;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.ptr.IntByReference;
+
+import org.lwjgl.glfw.GLFWNativeWin32;
+
+public class App extends Application {
+    public interface Dwmapi extends Library {
+        Dwmapi INSTANCE = Native.load("dwmapi", Dwmapi.class);
+        int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        void DwmSetWindowAttribute(HWND hwnd, int dwAttribute, IntByReference pvAttribute, int cbAttribute);
+    }
+
+    @Override
+    protected void configure(Configuration config) {
+        config.setTitle("Java ImGUI Custom Titlebar");
+    }
+
+    @Override
+    protected void preRun() {
+        super.preRun();
+
+        this.colorBg.set(0.13f, 0.145f, 0.16f, 1.0f);
+
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            try {
+                long glfwWindow = this.handle;
+
+                long hwndLong = GLFWNativeWin32.glfwGetWin32Window(glfwWindow);
+                HWND hwnd = new HWND(new Pointer(hwndLong));
+
+                IntByReference pvAttribute = new IntByReference(1);
+                Dwmapi.INSTANCE.DwmSetWindowAttribute(
+                        hwnd,
+                        Dwmapi.DWMWA_USE_IMMERSIVE_DARK_MODE,
+                        pvAttribute,
+                        4
+                );
+            } catch (Exception e) {
+                System.err.println("Konnte Dark Titlebar nicht setzen: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void process() {
+
+    }
+
+    public static void main(String[] args) {
+        launch(new App());
+    }
+}
